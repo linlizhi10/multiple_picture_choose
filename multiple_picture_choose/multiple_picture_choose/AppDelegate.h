09fac97c7f6d002c09e021609b5002f8dad5eb73
linlizhi10@163.com <linlizhi10@163.com>
2015-04-29 20:59:17 +0800

    checkbutton

---
+++ multiple_picture_choose/multiple_picture_choose/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  multiple_picture_choose
+//
+//  Created by skunk  on 15/4/29.
+//  Copyright (c) 2015年 linlizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ multiple_picture_choose/multiple_picture_choose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multiple_picture_choose
+//
+//  Created by skunk  on 15/4/29.
+//  Copyright (c) 2015年 linlizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
